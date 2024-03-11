d0f19fbbb272149cb848cd5e3bab5514fe85b351
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-03-11 12:30:41 +0300

    style: Update Tailwind CSS configuration
    Refined Tailwind CSS configuration to include additional file types in the 'content' property and extended the theme with custom styles. Added new fonts, color palette, animations, and responsive breakpoints for improved styling and design consistency.

--- Innovators/tailwind.config.js
+++ Innovators/tailwind.config.js
@@ -2,9 +2,35 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
+    "./src/**/*.{html,js,ts,jsx,tsx}"],
+    theme: {
+      fontFamily: {
+        Poppins: "Poppins"
+      },
+      extend: {
+        colors: {
+          Teal: "#2F6C6D",
+          HummingBird:"#D1F1EE",
+          yellow:"#e4d63b",
+          Solitude:"#e9e9ea",
+          gray:"#4B4B4C"
+        },
+        animation: {
+          slide: "slide 25s linear infinite"
+        },
+        keyframes: {
+          slide: {
+            "0%, 100%" : {transform: "translateX(5%)"},
+            "50%" : {transform: "translateX(-125%)"}
+          }
+        }
+      },
+      screens: {
+        xs: "480px",
+        sm: "768",
+        md: "1060px"
+      },
+    },
+  
   plugins: [],
 }
